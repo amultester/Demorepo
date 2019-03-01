2f8a5c59fb7e3b67ad53e61e91648908ccaa979c
amultester <testeramul@gmail.com>
2019-03-01 13:13:11 +0530

    First commit by amul

---
+++ Excel_read_write/src/Read_write/Excel_read_write.java
@@ -0,0 +1,35 @@
+package Read_write;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FilterInputStream;
+import java.io.FilterOutputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Excel_read_write {
+	
+	public static void main (String[]args) throws Exception {
+	
+		File src= new File ("C:\\Users\\innobot-user-1.LAPTOP-9DDO4JSH\\Downloads\\Users-Bulk-Import-Status (1).xlsx");
+		
+		FileInputStream input= new FileInputStream(src);
+		XSSFWorkbook wb= new XSSFWorkbook(input);
+		XSSFSheet sh1 = wb.getSheetAt(0);
+	    String	 sheetvalue = sh1.getRow(0).getCell(0).getStringCellValue();
+        System.out.println("The excel value is" +sheetvalue );
+       
+       
+       //write
+       
+      sh1.getRow(0).createCell(17).setCellValue("25");
+      FileOutputStream src1 = new FileOutputStream (new File("C:\\Users\\innobot-user-1.LAPTOP-9DDO4JSH\\Downloads\\Untitled 4.xlsx"));
+      wb.write(src1);
+      System.out.println("The new value is entered ");
+		
+	}
+}
+
